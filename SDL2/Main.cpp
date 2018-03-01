6a4d708f5c332567d82d3cde152612a0f310c987
Albertito029 <36154189+Albertito029@users.noreply.github.com>
2018-03-01 13:09:50 +0100

    adjustments

--- SDL2/Main.cpp
+++ SDL2/Main.cpp
@@ -1,6 +1,5 @@
 #include "SDL/Include/SDL.h"
-#include <iostream>
-using namespace std;
+
 #pragma comment(lib, "SDL/Libraries/SDL2.lib")
 #pragma comment(lib, "SDL/Libraries/SDL2main.lib")
 
@@ -13,30 +12,24 @@ int main(int argc, char* args[])
 	rect.h = 70;
 	rect.x = 0;
 	rect.y = 0;
-	SDL_Event e;
 	const int SCREEN_WIDTH = 1280, SCREEN_HEIGHT = 680;
 	int dirx = 1, diry = 1;
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
+		
 	}
 	else
 	{
 		window = SDL_CreateWindow("Red Box", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_BORDERLESS /*SDL_WINDOW_SHOWN*/);
 		if (window == NULL)
 		{
-			cout << "Window could not be created! SDL_Error: " << SDL_GetError() << endl;
+			
 		}
 		else
 		{
 			blockRender = SDL_CreateRenderer(window, -1, 0);
 			
 			while (1) {
-				if (SDL_PollEvent(&e)) {
-					if (e.type == SDL_QUIT) {
-						break;
-					}
-				}
 				while (rect.x <= 1280 && rect.x >= 0 && rect.y >= 0 && rect.y <= 680) {
 					rect.x = rect.x + dirx;
 					if (rect.x == 1200 || rect.x == 0) {
@@ -51,7 +44,7 @@ int main(int argc, char* args[])
 					SDL_SetRenderDrawColor(blockRender, 255, 0, 0, 255);
 					SDL_RenderFillRect(blockRender, &rect);
 					SDL_RenderPresent(blockRender);
-					SDL_Delay(2.5);
+					SDL_Delay(2);
 				}			
 			}
 		}
