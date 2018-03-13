56f8aabf966c2bb7e8fccb08c7bdff159989ca4a
Albertito029 <36154189+Albertito029@users.noreply.github.com>
2018-03-13 17:12:37 +0100

    bool for keys + blast movement

--- SDL2/Main.cpp
+++ SDL2/Main.cpp
@@ -10,93 +10,116 @@ int main(int argc, char* args[])
 	SDL_Window* window = NULL;
 	SDL_Renderer* blockRender = NULL;
 	SDL_Event e;
+	SDL_Init(SDL_INIT_VIDEO);
 	SDL_Rect rect;
 	rect.w = 80;
 	rect.h = 70;
 	rect.x = 250;
 	rect.y = 250;
-	SDL_Rect blast;
-	blast.w = 30;
-	blast.h = 10;
-	blast.x = -250;
-	blast.y = -250;
-	SDL_Surface *background;
-	background = IMG_Load("");
-	const int SCREEN_WIDTH = 1280, SCREEN_HEIGHT = 680;
-	enum keys {UP, DOWN, LEFT, RIGHT, SPACE};
-	//bool keys = { 0, 0, 0, 0, 0 };
-	bool quit = false;
-	
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-	}
-	else
-	{
-		window = SDL_CreateWindow("Red Box", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
+	SDL_Rect blast[1];
+	int vel = 1, shots = 0, i;
+	bool quit = false, mleft = false, mright = false, mup = false, mdown = false;
+	window = SDL_CreateWindow("Red Box", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 680, SDL_WINDOW_BORDERLESS);
+	blockRender = SDL_CreateRenderer(window, -1, 0);
+	while (!quit) {
+		while (SDL_PollEvent(&e) != 0)
 		{
-		}
-		else
-		{
-			blockRender = SDL_CreateRenderer(window, -1, 0);
-			while (!quit) {
-				while (SDL_PollEvent(&e))
+			if (e.type == SDL_KEYUP)
+			{
+				switch (e.key.keysym.sym)
 				{
-					switch (e.type)
-					{
-					case SDL_QUIT:
-						quit = true;
-						break;
-					case SDL_KEYDOWN:
-						switch (e.key.keysym.sym)
-						{
-						case SDLK_ESCAPE:
-							quit = true;
-							break;
-						case SDLK_LEFT:
-							rect.x -= 5;
-							break;
-						case SDLK_RIGHT:
-							rect.x += 5;
-							break;
-						case SDLK_UP:
-							rect.y -= 5;
-							break;
-						case SDLK_DOWN:
-							rect.y += 5;
-							break;
-						case SDLK_SPACE:
-							blast.x = rect.x + 80;
-							blast.y = rect.y + 30;
-							break;
-						default:
-							break;
-						}
-					}
+				case SDLK_LEFT:
+					mleft = false;
+					break;
+				case SDLK_RIGHT:
+					mright = false;
+					break;
+				case SDLK_UP:
+					mup = false;
+					break;
+				case SDLK_DOWN:
+					mdown = false;
+					break;
+				case SDLK_SPACE:
+					
+					break;
 				}
-				SDL_SetRenderDrawColor(blockRender, 0, 0, 255, 255);
-				SDL_RenderClear(blockRender);
-				SDL_SetRenderDrawColor(blockRender, 255, 0, 0, 255);
-				SDL_RenderFillRect(blockRender, &rect);
-				SDL_SetRenderDrawColor(blockRender, 0, 255, 0, 255);
-				SDL_RenderFillRect(blockRender, &blast);
-				SDL_RenderPresent(blockRender);
+			}
+			if (e.type == SDL_KEYDOWN)
+			{
+				switch (e.key.keysym.sym)
+				{
+				case SDLK_LEFT:
+					mleft = true;
+					break;
+				case SDLK_RIGHT:
+					mright = true;
+					break;
+				case SDLK_UP:
+					mup = true;
+					break;
+				case SDLK_DOWN:
+					mdown = true;
+					break;
+				case SDLK_ESCAPE:
+					quit = true;
+					break;
+				case SDLK_SPACE:
+					blast[shots].x = rect.x + 80;
+					blast[shots].y = rect.y + 30;
+					shots++;
+					break;
+				}
 			}
 		}
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-		return 0;
+		if (rect.x == 0) {
+			mleft = false;
+		}
+		if (rect.x == 1200) {
+			mright = false;
+		}
+		if (rect.y == 0) {
+			mup = false;
+		}
+		if (rect.y == 610) {
+			mdown = false;
+		}
+		if (mleft == true) {
+			rect.x -= vel;
+			
+		}
+		if (mright == true) {
+			rect.x += vel;
+			
+		}
+		if (mup == true) {
+			rect.y -= vel;
+			
+		}
+		if (mdown == true) {
+			rect.y += vel;
+			
+		}
+		SDL_SetRenderDrawColor(blockRender, 0, 255, 0, 255);
+		for (int i = 0; i <= shots; ++i)
+		{
+			blast[i].w = 30;
+			blast[i].h = 10;
+			blast[i].x += vel;
+			SDL_RenderFillRect(blockRender, &blast[i]);
+			if (blast[i].x < 1280)
+			{
+				SDL_RenderCopy(blockRender, NULL, NULL, &blast[i]);
+			}
+		}
+		SDL_RenderPresent(blockRender);
+		SDL_SetRenderDrawColor(blockRender, 0, 0, 255, 255);
+		SDL_RenderClear(blockRender);
+		SDL_SetRenderDrawColor(blockRender, 255, 0, 0, 255);
+		SDL_RenderFillRect(blockRender, &rect);
+		SDL_RenderPresent(blockRender);
 	}
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
 }
-/* REBOTE
-int dirx = 1, diry = 1;
--- while (rect.x <= 1280 && rect.x >= 0 && rect.y >= 0 && rect.y <= 680) -- 
-rect.x = rect.x + dirx;
-if (rect.x == 1200 || rect.x == 0) {
-dirx = dirx * -1;
-}
-rect.y = rect.y + diry;
-if (rect.y == 0 || rect.y == 610) {
-diry = diry * -1;
-}
-*/
